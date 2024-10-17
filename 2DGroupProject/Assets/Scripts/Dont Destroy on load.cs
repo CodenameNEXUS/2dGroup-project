d15a0420d271f7e950474980dd2304f5d0232a58
CodenameNEXUS <puggaming456@gmail.com>
2024-10-17 13:35:02 -0700

    fixed invisible bug

--- 2DGroupProject/Assets/Scripts/Dont Destroy on load.cs
+++ 2DGroupProject/Assets/Scripts/Dont Destroy on load.cs
@@ -1,15 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyonload : MonoBehaviour
 {
     GameObject SpawnPos;
+    GameObject menu;
+    GameObject player;
+    Component Renderer;
+
+    public string levelToLoad = "Landon TEST";
+    public bool OriginalPlayer = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        menu = GameObject.FindGameObjectWithTag("Menu");
+        player = GameObject.FindGameObjectWithTag("Player");
         SpawnPos = GameObject.FindGameObjectWithTag("SpawnPos");
+        if (player != null)
+        {
+            OriginalPlayer = true;
+        }
+        menu = GameObject.FindGameObjectWithTag("Menu");
+        if (menu != null)
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponentInChildren<Canvas>().enabled = false;
+        }
+        if (menu == null)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+            GetComponentInChildren<Canvas>().enabled = true;
+        }
     }
 
     // Update is called once per frame
@@ -28,7 +53,23 @@ public class DontDestroyonload : MonoBehaviour
     }
     private void OnLevelWasLoaded(int level)
     {
+        menu = GameObject.FindGameObjectWithTag("Menu");
+        if (menu != null)
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponentInChildren<Canvas>().enabled = false;
+        }
+        if (menu == null)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+            GetComponentInChildren<Canvas>().enabled = true;
+        }
+        if (OriginalPlayer == false)
+        {
+            Destroy(gameObject);
+        }
         SpawnPos = GameObject.FindGameObjectWithTag("SpawnPos");
         gameObject.transform.position = SpawnPos.transform.position;
+        menu = GameObject.FindGameObjectWithTag("Menu");
     }
 }
