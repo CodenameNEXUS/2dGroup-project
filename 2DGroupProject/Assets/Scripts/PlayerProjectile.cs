cf0c283c39a8b021d4e30dce6a3a69b0f75a73c8
CodenameNEXUS <puggaming456@gmail.com>
2024-10-29 13:55:45 -0700

    made some changes to the start menu and cave lvls

---
+++ 2DGroupProject/Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour, IDamageable
+{
+    [SerializeField] private float damageAmount = 7f;
+    private IDamageable idamageable;
+
+    private Collider2D coll;
+
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        idamageable = collision.gameObject.GetComponent<IDamageable>();
+        if (idamageable != null)
+        {
+            //Damage
+            idamageable.Damage(damageAmount);
+        }
+    }
+
+
+
+    public void Damage(float damageAmount)
+    {
+        throw new System.NotImplementedException();
+    }
+}
